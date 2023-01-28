4dfda97146a0e9582ba2757d3d51e09f7d721e48
icarocaiquecaiq <icarocaiquevp16@gmail.com>
2023-01-28 11:47:18 -0300

    first

---
+++ app.js
@@ -0,0 +1,22 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const botao = document.getElementById("btn") 
+const cor = document.querySelector(".color") // ponto = classe
+ 
+botao.addEventListener('click', function change () { 
+    
+    const CorAle = pegarnumale() 
+    console.log(CorAle)
+    document.body.style.backgroundColor= colors[CorAle]
+    cor.textContent = colors[CorAle]
+
+   // agora, eu quero escolher um número aleatório para mudar a cor //
+   // para isso,é necessário uma função que gera números aleatórios//
+   
+   
+   
+})
+
+
+function pegarnumale() {
+    return Math.floor(Math.random() * colors.length)
+}
