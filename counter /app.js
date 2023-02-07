7dfc62ac9baeb9db5c1e917e4d0f154189fca54a
icarocaiquecaiq <icarocaiquevp16@gmail.com>
2023-02-07 11:30:00 -0300

    changes

--- counter /app.js
+++ counter /app.js
@@ -4,12 +4,27 @@ var contTela = document.querySelector('#value')
 var botao = document.querySelectorAll('.btn')
 
 botao.forEach(function (btn) { // parametro "btn" registra cada botao //
- 
-    btn.addEventListener('click',function (e) {
+
+   
+    btn.addEventListener("dblclick",function (e) {
+        var estilo = (e.currentTarget.classList)
+
+        if (estilo.contains('increase') && contador > 0) {
+            
+            contador = (contador - 2) * 2
+            console.log(contador)
+            console.log("teste")
+
+            
+    }contTela.textContent = contador})    
+
+
+   btn.addEventListener('click',function (e) {
       
-        const estilo = (e.currentTarget.classList)
-
-
+        var estilo = (e.currentTarget.classList)
+        
+        
+             
            
         if (estilo.contains('decrease') ) {
             
@@ -21,7 +36,15 @@ botao.forEach(function (btn) { // parametro "btn" registra cada botao //
             contador ++ 
             console.log(contador)
             
-        } else {
+        } else if (estilo.contains("-100")) {
+            contador = contador - 100
+            
+            
+        } else if (estilo.contains("+100")){
+        
+            contador += 100 
+        
+        }else {
             
             contador = 0 
         }
@@ -29,21 +52,32 @@ botao.forEach(function (btn) { // parametro "btn" registra cada botao //
         if (contador < 0) {
 
             contTela.style.color= "var(--clr-red-dark)"
-
+            document.body.style.backgroundColor= ("rgb("+diferentRed(10,10)+",87,87)")
+            console.log(diferentRed(10,10))
         }else if (contador > 0) {
 
             contTela.style.color="var(--clr-green-dark)"
-
+            document.body.style.backgroundColor= ("rgb( "+diferentRed()+", 252, 132)")
+            console.log(diferentRed(29+1))
         }else{
 
             contTela.style.color="var(--clr-primary-1)"
+            document.body.style.backgroundColor= ("white")
         }
 
-        contTela.textContent=contador
+        contTela.textContent = contador
 
     })
-        
+         
 })
 
+function diferentRed(a,b) {
+    var teste = a+b
+    if (teste == 20){
+        return Math.floor(Math.random()*(450-200)+200)
+    }else {
+        return Math.floor(Math.random()*(127-60)+60)}
 
+}
 
+console.log(diferentRed())
